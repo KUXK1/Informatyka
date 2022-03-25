5ae6c91b85df4dcfc8fd9f32b5757ec2a72fd23a
KUXK1 <maciejpodhajnykolo2@gmail.com>
2022-03-25 14:12:16 +0100

    LIczby pierwsze

--- Liczby pierwsze/Liczby pierwsze.cpp
+++ Liczby pierwsze/Liczby pierwsze.cpp
@@ -6,15 +6,33 @@ using namespace std;
 int main()
 {
     int n;
-	int x;
+	int lp=0;
+	
+	int p = 2;
+	int dot = 0;
+	
+
 	
 	cout << " ile licz pierwszych";
     cin >> n;
-	for (int i = 0; i <=  n; i++)
+	cout << " liczby pierwsze to:";
+	while(lp < n)
 	{
+		dot = 0;
+		for (int d = 2; d <= p - 1; d++) 
+		{
+			if (p % d == 0) {
+				dot =  1;
+			}
+
+		}
 		
-		x = i/;
+		if (dot == 0) {
+			cout << p << ",";
+			lp = lp + 1;
+		}
 		
+		p = p + 1;
 	}
 
 }
