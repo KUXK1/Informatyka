3be07fe00f077f9275c1e4a16e382f863284231d
KUXK1 <maciejpodhajnykolo2@gmail.com>
2022-04-01 13:51:51 +0200

    nwd

---
+++ Najwiekszy wspólny dzielnik/Najwiekszy wspólny dzielnik/Najwiekszy wspólny dzielnik.cpp
@@ -0,0 +1,28 @@
+﻿// Najwiekszy wspólny dzielnik.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    int b;
+    cout << "Podaj liczbe a";
+    cin >> a;
+    cout << "Podaj liczbe b";
+    cin >> b;
+    while (a != b)
+    {
+        if (a > b)
+        {
+            a = a - b;
+        }
+        if (b > a)
+        {
+            b = b - a;
+        }
+    }
+    cout << a << " Jest najwiekszym wspólnymd dzielnikiem";
+}
+
+
